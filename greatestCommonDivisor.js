a32545c67a55ef4fdc4bed41f41ca7f9bc292f18
Blaise Iradukunda <iradub0@sewanee.edu>
2017-08-20 23:58:20 -0500

    Update greatestCommonDivisor.js

--- greatestCommonDivisor.js
+++ greatestCommonDivisor.js
@@ -1,2 +1,43 @@
-function prime(){
+function isPrime(number) {
+    if (number % 2 == 0)
+        return false;
+    for (var i = 3;(i * i) <= number; i += 2) {
+        if (number % i == 0)
+            return false;
+    }
+    return true;
 }
+
+function nextPrime(number) {
+    var i = number;
+    while (true) {
+        i++;
+        if (isPrime(i)) return i;
+    }
+}
+
+function findPrimeDivisors(el){
+  var mapDivs = {
+    2 : 0
+  };
+  
+  var divKeys = Object.keys(mapDivs);
+  for(var i = 2; i < el/2; i++ ){
+    while(el % i === 0){
+      mapDivs[i] ++;
+      el = el/i;
+    }
+    console.log(mapDivs);
+    return mapDivs;
+  }
+}
+function smallestCommons(arr) {
+  var divs = [];
+  for(var i=0; i<arr.length; i++){
+    divs[i] = findPrimeDivisors(arr[i]);
+  }
+  return arr;
+}
+
+
+smallestCommons([16,8]);
